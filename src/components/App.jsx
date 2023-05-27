70d41086a067a37d32668b3c55be5802bae188ed
PaulPonomarenko <pashaponomarenkoa1999@gmail.com>
2023-05-27 21:10:24 +0300

    оновлює перше завдання

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,17 @@
 import user from 'data/user';
-import css from './App.module.css';
-import { ProfileDescription } from './profile/ProfileDescription';
-import { ProfileStats } from './stats/ProfileStats';
+import { Profile } from './profile/Profile';
 import { Statistics } from './statistics/Statistics';
 import data from 'data/data';
 export const App = () => {
   return (
     <>
-      <div className={css.profile}>
-        <ProfileDescription
-          avatar={user.avatar}
-          location={user.location}
-          tag={user.tag}
-          username={user.username}
-        />
-        <ProfileStats
-          followers={user.stats.followers}
-          views={user.stats.views}
-          likes={user.stats.likes}
-        />
-      </div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics title="Upload stats" stats={data} />
     </>
   );
